d88b613beef84b03407e082b3279d83f709c3140
Karol Orzechowski <karol.orzechowski1337@gmail.com>
2024-12-17 22:19:19 +0100

    Adds citizen front

---
+++ frontend/citizen/citizen_app.py
@@ -0,0 +1,34 @@
+import flet as ft
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'database')))
+
+from services import SERVICE_OPTIONS
+
+def main(page: ft.Page):
+    page.title = "Formularz zgłoszenia zapotrzebowania"
+    page.vertical_alignment = ft.MainAxisAlignment.START
+
+    name_input = ft.TextField(label="Imię i nazwisko", autofocus=True)
+    service_input = ft.Dropdown(
+        label="Usługa",
+        options=[ft.dropdown.Option(name) for name in SERVICE_OPTIONS],
+    )
+    description_input = ft.TextField(label="Opis zapotrzebowania", multiline=True, min_lines=3, max_lines=5)
+
+    def submit_form(e):
+        name = name_input.value
+        service = service_input.value
+        description = description_input.value
+        
+        print(f"Nowe zapotrzebowanie:\nImię: {name}\nUsługa: {service}\nOpis: {description}")
+        text = ft.Text(f"Dziękujemy za zgłoszenie zapotrzebowania!")
+        text.padding = ft.padding.all(20)
+        page.add(text)
+
+    submit_button = ft.ElevatedButton("Wyślij zapotrzebowanie", on_click=submit_form)
+
+    page.add(name_input, service_input, description_input, submit_button)
+
+ft.app(target=main)
