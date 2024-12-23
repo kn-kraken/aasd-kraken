e086af82c02a7e3a777880ae28ecce4394de2a0a
Jeremi Sobierski <tbuli1112@gmail.com>
2024-12-23 13:11:55 +0100

    hub WIP

--- agents/hub/main.py
+++ agents/hub/main.py
@@ -3,67 +3,79 @@ from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour
 from spade.template import Template
 import json
+from dataclasses import dataclass
+
+
+@dataclass
+class RentalOffer:
+    price: int
+    location: tuple[float, float]
+
+
+@dataclass
+class RentalRequest:
+    price: int
 
 
 class HubAgent(Agent):
-    class OpenOfferRequestRecvBehav(CyclicBehaviour):
-        async def run(self):
-            print("OpenOfferRequestRecvBehav running")
-            msg = await self.receive(timeout=20)
-            if msg:
-                print("Message received with content: {}".format(msg.body))
-                offer = json.loads(msg.body)
-                print(offer["price"])
-            else:
-                print("Did not received any message after 10 seconds")
+    def __init__(self):
+        self.rental_offers = []
+        self.rental_requests = []
 
-    class ServiceDemandRequestRecvBehav(CyclicBehaviour):
+    class ServiceDemandRequestRecvBhv(CyclicBehaviour):
+        def __init__(self, hub):
+            self.hub = hub
+
         async def run(self):
-            print("OpenOfferRequestRecvBehav running")
+            print("OpenOfferRequestRecvBhv running")
             msg = await self.receive(timeout=20)
             if msg:
                 print("Message received with content: {}".format(msg.body))
             else:
                 print("Did not received any message after 10 seconds")
 
-    class StartNegotiationRecvBehav(CyclicBehaviour):
+        metadata = {
+            "performative": "inform",
+            "conversation_id ": "ServiceDemandRequest",
+        }
+
+    class RegisterRentalOfferRecvBhv(CyclicBehaviour):
+        def __init__(self, hub):
+            self.hub = hub
+
         async def run(self):
-            print("StartNegotiationRecvBehav running")
             msg = await self.receive(timeout=20)
-            if msg:
-                print("Message received with content: {}".format(msg.body))
-            else:
-                print("Did not received any message after 10 seconds")
+            data = json.loads(msg.body)
+            self.rental_offers.append(RentalOffer(**data))
+
+        metadata = {
+            "performative": "inform",
+            "conversation_id ": "RegisterRentalOffer",
+        }
+
+    class RegisterRentalRequestRecvBhv(CyclicBehaviour):
+        def __init__(self, hub):
+            self.hub = hub
+
+        async def run(self):
+            msg = await self.receive(timeout=20)
+            data = json.loads(msg.body)
+            self.rental_requests.append(RentalRequest(**data))
+
+        metadata = {
+            "performative": "inform",
+            "conversation_id ": "RegisterRentalRequest",
+        }
 
     async def setup(self):
         print("HubAgent started")
-        self.add_behaviour(
-            self.OpenOfferRequestRecvBehav(),
-            Template(
-                metadata={
-                    "performative": "propose",
-                    "conversation_id ": "OpenOfferRequest",
-                }
-            ),
-        )
-        self.add_behaviour(
-            self.ServiceDemandRequestRecvBehav(),
-            Template(
-                metadata={
-                    "performative": "inform",
-                    "conversation_id ": "ServiceDemandRequest",
-                }
-            ),
-        )
-        self.add_behaviour(
-            self.ServiceDemandRequestRecvBehav(),
-            Template(
-                metadata={
-                    "performative": "propose",
-                    "conversation_id ": "StartNegotiation",
-                }
-            ),
-        )
+
+        for attr in self.__dict__.values():
+            if (
+                isinstance(attr, CyclicBehaviour)
+                and (metadata := getattr(attr, "metadata", None)) is not None
+            ):
+                self.add_behaviour(attr(self), Template(metadata=metadata))
 
 
 async def main():
