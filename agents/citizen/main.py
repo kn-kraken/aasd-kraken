02b0bed149228d02af5c29982418d0392f2c665d
Jakub Kowieski <kubakowieski5151@gmail.com>
2024-12-26 02:59:26 +0100

    kowies city change

--- agents/citizen/main.py
+++ agents/citizen/main.py
@@ -2,40 +2,48 @@ import spade
 from spade.agent import Agent
 from spade.behaviour import OneShotBehaviour
 from spade.message import Message
-
+import json
 
 class CitizenAgent(Agent):
     class ServiceDemandRequest(OneShotBehaviour):
+        def __init__(self, localization, service_type, priority):
+            super().__init__()
+            self.localization = localization
+            self.service_type = service_type
+            self.priority = priority
+
+
         async def run(self):
             print("ServiceDemandRequest running")
             msg = Message(
                 to="hub_agent@localhost",
                 metadata={
                     "performative": "inform",
-                    "conversation_id ": "ServiceDemandRequest",
+                    "conversation_id": "ServiceDemandRequest",
                 },
-                body="żabka",
+                body=json.dumps({
+                    "localization": self.localization,
+                    "service_type": self.service_type,
+                    "priority": self.priority,
+                }),
             )
 
             await self.send(msg)
             print("Message sent!")
-
             await self.agent.stop()
 
-    async def setup(self):
-        print("CitizenAgent started")
-        b = self.ServiceDemandRequest()
-        self.add_behaviour(b)
-
+    def add_service_demand_request(self, localization, service_type, priority):
+        behavior = self.ServiceDemandRequest(localization, service_type, priority)
+        self.add_behaviour(behavior)
 
 async def main():
-    citizen_agent = CitizenAgent("citizen_agent@localhost", "citizen_agent_password")
-    await citizen_agent.start(auto_register=True)
-    print("citizen_agent started")
+    agent = CitizenAgent("citizen_agent@localhost", "citizen_agent_password")
+    await agent.start(auto_register=True)
+    agent.add_service_demand_request([1.0, 10.2], "Żabka", "High")
 
-    await spade.wait_until_finished(citizen_agent)
-    print("Agents finished")
+    await spade.wait_until_finished(agent)
 
+    await agent.stop()
 
 if __name__ == "__main__":
     spade.run(main())
